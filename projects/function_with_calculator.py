f944e5c958d89b379b892b346783ca387968a842
Suraj mishra <140611249+surajmishra-001@users.noreply.github.com>
2025-11-07 18:45:53 +0530

    Add initial implementation of a calculator with basic operations

---
+++ projects/function_with_calculator.py
@@ -0,0 +1,54 @@
+import pyfiglet
+
+result = pyfiglet.figlet_format("Calculator")
+print(result)
+
+msg = """
+Select operation:
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Exit
+"""
+
+def add(a,b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+
+while True:
+    operation = input(msg)
+
+    if operation == "5":
+        print("Exiting the calculator. Goodbye!")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    
+
+    result = 0
+
+    if operation == "1":
+        result = add(num1, num2)
+    elif operation == "2":
+        result = subtract(num1, num2)
+    elif operation == "3":
+        result = multiply(num1, num2)
+    elif operation == "4":
+        result = divide(num1, num2)
+    else:
+        print("Invalid operation")
+        continue
+    print("==============================")
+    print("Result:", result)
+    print("==============================")
